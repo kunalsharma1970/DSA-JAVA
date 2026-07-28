a5d4673992da431f045ad1a7ff9be36875cd277c
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-07-28 18:04:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (27.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low=0; int high=nums.length-1; int last=nums[nums.length-1];
-        int res=-1;
+        int first=nums[0];
         while(low<=high){
             int guess=(low+high)/2;
             if(nums[guess]>last){
-                low=guess+1;
+                if(nums[guess]==target) return guess;
+                else if(nums[guess]<target) low=guess+1;
+                else{
+                    if(first>target) low=guess+1;
+                    else high=guess-1;
+                }
             }else{
-                res=guess;
-                high=guess-1;
+                if(nums[guess]==target) return guess;
+                else if(nums[guess]>target) high=guess-1;
+                else{
+                    if(last<target) high=guess-1;
+                    else low=guess+1;
+                }
             }
-        }
-        int low1=0; int high1=res-1; 
-        while(low1<=high1){
-            int guess=(low1+high1)/2;
-            if(nums[guess]==target){
-                return guess;
-            }else if(nums[guess]>target){
-                high1=guess-1;
-            }else{
-                low1=guess+1;
-            }
-        }
-        int low2=res; int high2=nums.length-1;
-        while(low2<=high2){
-            int guess=(low2+high2)/2;
-            if(nums[guess]==target) return guess;
-            else if(nums[guess]>target) high2=guess-1;
-            else low2=guess+1;
         }
         return -1;
     }
