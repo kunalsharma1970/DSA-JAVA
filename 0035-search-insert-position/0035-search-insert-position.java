f099998126d7402060954cb1b41d3dd8332b3b26
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-07-29 10:19:52 +0530

    Time: 0 ms (100%), Space: 44.7 MB (67.11%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0; int high=nums.length-1;
+        while(low<=high){
+            int guess=(low+high)/2;
+            if(nums[guess]==target) return guess;
+            else if(nums[guess]>target) high=guess-1;
+            else low=guess+1;
+        }
+        return low;
+    }
+}
