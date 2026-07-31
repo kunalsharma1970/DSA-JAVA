d358191cb29b32c1bd58de1b92cd16f14f07fe22
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-07-31 17:31:41 +0530

    Time: 16 ms (38.38%), Space: 47.6 MB (67.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -11,33 +11,34 @@ class Solution {
         PriorityQueue<Pair> pq=new PriorityQueue<>(
             (a,b)->{
                 if(a.first!=b.first) return a.first-b.first;
-                return a.first-b.first;
+                return a.second-b.second;
             }
         );
-        HashMap<Integer,Integer> map =new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])) map.put(nums[i],map.get(nums[i])+1);
             else map.put(nums[i],1);
         }
         for(Map.Entry<Integer,Integer>e:map.entrySet()){
-            int freq=e.getValue();
             int element=e.getKey();
-            
-            Pair p=new Pair(freq,element);
-            if(pq.size()<k) {
-                pq.add(p);
+            int freq=e.getValue();
+
+            Pair curr=new Pair(freq,element);
+            if(pq.size()<k){
+                pq.add(curr);
                 continue;
             }
-            if(p.first>pq.peek().first){
+            if(curr.first<pq.peek().first) continue;
+            else{
                 pq.poll();
-                pq.add(p);
-            }else continue;
+                pq.add(curr);
+            }
         }
-        int[]res=new int[k]; int s=0;
+        int []res=new int[k];
+        int s=0;
         while(!pq.isEmpty()){
-            res[s]=pq.peek().second;
+            res[s]=pq.poll().second;
             s++;
-            pq.poll();
         }
         return res;
     }
