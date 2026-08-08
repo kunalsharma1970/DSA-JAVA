74009bc8c15814a8b8da62b24dd0b907107efcf8
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-08-08 23:23:43 +0530

    Time: 26 ms (9.26%), Space: 97.9 MB (83.78%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void fun(int n,int k,int start,List<List<Integer>>list,List<Integer>temp){
+        if(temp.size()==k){
+            list.add(new ArrayList<>(temp));
+        }
+        for(int i=start;i<=n;i++){
+            temp.add(i);
+            fun(n,k,i+1,list,temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> list = new ArrayList<>();
+        fun(n,k,1,list,new ArrayList<>());
+        return list;
+    }
+}
