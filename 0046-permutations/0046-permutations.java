631d6e959a8570a0c8e4269e062ab518e8b800e7
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-08-09 12:09:15 +0530

    Time: 1 ms (87.1%), Space: 45.5 MB (46.16%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,26 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>list= new ArrayList<>();
-        gen(0,nums,list);
-        return list;
-    }
-    public static void gen(int start,int[]nums,List<List<Integer>>list){
+    public static void fun(int start,int []nums,List<List<Integer>> list){
         if(start==nums.length){
-            ArrayList<Integer> temp=new ArrayList<>();
-            for(int i:nums){
-                temp.add(i);
+            ArrayList<Integer> temp= new ArrayList<>();
+            for(int num:nums){
+                temp.add(num);
             }
             list.add(new ArrayList<>(temp));
             return;
         }
         for(int i=start;i<nums.length;i++){
-            int temp=nums[i];
-            nums[i]=nums[start];
-            nums[start]=temp;
-            gen(start+1,nums,list);
-            temp=nums[i];
-            nums[i]=nums[start];
-            nums[start]=temp;
+        int temp=nums[i];
+        nums[i]=nums[start];
+        nums[start]=temp;
+        fun(start+1,nums,list);
+        temp=nums[i];
+        nums[i]=nums[start];
+        nums[start]=temp;
         }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list= new ArrayList<>();
+        fun(0,nums,list);
+        return list;
     }
 }
