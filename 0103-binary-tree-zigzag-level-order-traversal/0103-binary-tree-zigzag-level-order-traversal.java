e7e267595910b4d6f0b359c73a35b2c48b7220b5
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-08-11 01:09:38 +0530

    Time: 1 ms (79.76%), Space: 43.7 MB (63.31%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -22,28 +22,23 @@ class Solution {
         int level=0;
         while(!q.isEmpty()){
             int levelsize=q.size();
-            int[]res= new int[levelsize];
+            int []res=new int[levelsize];
             int first=0; int last=res.length-1;
             while(levelsize-->0){
+                TreeNode t=q.poll();
                 if(level==0){
-                    TreeNode t=q.poll();
                     res[first]=t.val;
                     first++;
-                    if(t.left!=null) q.add(t.left);
-                    if(t.right!=null) q.add(t.right);
                 }else{
-                    TreeNode t=q.poll();
                     res[last]=t.val;
                     last--;
-                    if(t.left!=null) q.add(t.left);
-                    if(t.right!=null)q.add(t.right);
                 }
+                if(t.left!=null) q.add(t.left);
+                if(t.right!=null) q.add(t.right);
             }
             level=1-level;
-            List<Integer> list= new ArrayList<>();
-            for(int num:res){
-                list.add(num);
-            }
+            List<Integer>list=new ArrayList<>();
+            for(int num:res)  list.add(num);
             ans.add(new ArrayList<>(list));
         }
         return ans;
