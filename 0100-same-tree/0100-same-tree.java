037fb794940aa24bb1281696700f111cb749d5ba
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-08-11 19:44:01 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.1%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,7 +18,6 @@ class Solution {
         if(p==null && q==null) return true;
         if(p==null && q!=null) return false;
         if(p!=null && q==null) return false;
-
         return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
