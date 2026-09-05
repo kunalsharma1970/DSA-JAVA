e0756631848c91fca7de0b298d180d2d7e755035
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-05 14:28:16 +0530

    Time: 3 ms (87.99%), Space: 52.3 MB (58.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,18 +3,20 @@ class Solution {
         int count=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1') count++;
-                dfs(grid,i,j);
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
             }
         }
         return count;
     }
-    public void dfs(char [][]grid,int row,int cols){
-        if(row<0 || cols<0 || row>=grid.length || cols>=grid[0].length || grid[row][cols]=='0') return;
-        grid[row][cols]='0';
-        dfs(grid,row+1,cols);
-        dfs(grid,row-1,cols);
-        dfs(grid,row,cols+1);
-        dfs(grid,row,cols-1);
+    public void dfs(char [][]grid,int r,int c){
+        if(r<0 || c<0 || r==grid.length || c==grid[0].length || grid[r][c]=='0')return;
+        grid[r][c]='0';
+        dfs(grid,r-1,c);
+        dfs(grid,r+1,c);
+        dfs(grid,r,c-1);
+        dfs(grid,r,c+1);
     }
 }
