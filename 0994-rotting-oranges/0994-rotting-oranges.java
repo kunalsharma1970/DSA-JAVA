4e52a5e401d2055430d038b30ddcf83ce913788b
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-05 22:37:45 +0530

    Time: 2 ms (88.83%), Space: 44.2 MB (55.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,39 +1,39 @@
 class Solution {
     class Pair{
-        int row;
-        int cols;
-        Pair(int row,int cols){
-            this.row=row;
-            this.cols=cols;
+        int first;
+        int second;
+        Pair(int first,int second){
+            this.first=first;
+            this.second=second;
         }
     }
     public int[]x={-1,1,0,0};
-    public int []y={0,0,-1,1};
+    public int[]y={0,0,-1,1};
+
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q= new LinkedList<>();
+        Queue<Pair>q=new LinkedList<>();
         int time=0; int fresh=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     q.add(new Pair(i,j));
                     grid[i][j]=0;
-                }
-                else if(grid[i][j]==1) fresh++;
+                }else if(grid[i][j]==1) fresh++;
             }
         }
         while(!q.isEmpty() && fresh>0){
             time++;
             int size=q.size();
             while(size-->0){
-                Pair p=q.poll();
-                int r=p.row;
-                int c=p.cols;
+                Pair p= q.poll();
+                int r=p.first;
+                int c=p.second;
                 for(int k=0;k<4;k++){
-                    int rr=r+x[k];
-                    int cc=c+y[k];
-                    if(valid(grid,rr,cc) && grid[rr][cc]==1){
-                        q.add(new Pair(rr,cc));
-                        grid[rr][cc]=0;
+                    int row= r+x[k];
+                    int cols=c+y[k];
+                    if(isValid(grid,row,cols) && grid[row][cols]==1){
+                        q.add(new Pair(row,cols));
+                        grid[row][cols]=0;
                         fresh--;
                     }
                 }
@@ -42,8 +42,8 @@ class Solution {
         if(fresh>0) return -1;
         return time;
     }
-    public boolean valid(int[][]grid,int r,int c){
-        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length) return false;
+    public boolean isValid(int[][]grid,int r,int c){
+        if(r<0||r>=grid.length||c<0||c>=grid[0].length) return false;
         return true;
     }
 }
