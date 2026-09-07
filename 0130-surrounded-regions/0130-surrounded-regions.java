6afd5ac698e7fc7221e822c3d7da51468bf205dd
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-08 00:05:41 +0530

    Time: 2 ms (75.99%), Space: 48 MB (22.22%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n=board.length;
+        int m=board[0].length;
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O') dfs(board,0,i);
+        }
+        for(int i=0;i<m;i++){
+            if(board[n-1][i]=='O') dfs(board,n-1,i);
+        }
+        for(int j=0;j<n;j++){
+            if(board[j][0]=='O') dfs(board,j,0);
+        }
+        for(int j=0;j<n;j++){
+            if(board[j][m-1]=='O') dfs(board,j,m-1);
+        }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]=='#') board[i][j]='O';
+                else board[i][j]='X';
+            }
+        }
+    }
+    boolean valid(char[][]board,int row,int cols){
+        if(row<0 || row>=board.length ||  cols<0 || cols>=board[0].length) return false;
+        return true;
+    }
+    public int[]x={1,-1,0,0};
+    public int[]y={0,0,1,-1};
+    public void dfs(char[][]board,int row,int cols){
+        board[row][cols]='#';
+        for(int k=0;k<4;k++){
+            int r=row+x[k];
+            int c=cols+y[k];
+            if(valid(board,r,c) && board[r][c]=='O'){
+                dfs(board,r,c);
+            }
+        }
+    }
+}
