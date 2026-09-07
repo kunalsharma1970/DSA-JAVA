657e370c8c35b901a948ef7cd96b664c22a29bb1
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-08 00:59:28 +0530

    Time: 1 ms (99.84%), Space: 48 MB (10.71%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,7 +1,6 @@
 class Solution {
     public void solve(char[][] board) {
-        int n=board.length;
-        int m=board[0].length;
+        int n=board.length; int m=board[0].length;
         for(int i=0;i<m;i++){
             if(board[0][i]=='O') dfs(board,0,i);
         }
@@ -14,15 +13,15 @@ class Solution {
         for(int j=0;j<n;j++){
             if(board[j][m-1]=='O') dfs(board,j,m-1);
         }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(board[i][j]=='#') board[i][j]='O';
                 else board[i][j]='X';
             }
         }
     }
-    boolean valid(char[][]board,int row,int cols){
-        if(row<0 || row>=board.length ||  cols<0 || cols>=board[0].length) return false;
+    public boolean valid(char[][]board,int row,int cols){
+        if(row<0 || row>=board.length || cols<0 || cols>=board[0].length) return false;
         return true;
     }
     public int[]x={1,-1,0,0};
@@ -32,9 +31,7 @@ class Solution {
         for(int k=0;k<4;k++){
             int r=row+x[k];
             int c=cols+y[k];
-            if(valid(board,r,c) && board[r][c]=='O'){
-                dfs(board,r,c);
-            }
+            if(valid(board,r,c) && board[r][c]=='O') dfs(board,r,c);
         }
     }
 }
