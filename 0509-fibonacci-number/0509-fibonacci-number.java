dfa9b7eafea4fc090e74069acc4b6e32bbf6add3
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-14 11:05:34 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.41%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int []dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fun(n,dp);
+    }
+    public int fun(int n,int[]dp){
+        if(n<2){
+            return n;
+        }
+        if(dp[n]!=-1) return dp[n];
+        int a=fun(n-1,dp);
+        int b=fun(n-2,dp);
+        dp[n]=a+b;
+        return a+b;
+    }
+}
