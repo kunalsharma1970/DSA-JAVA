aae52fbd4f283fdab7dcd0eec4a54a195b4c772d
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-17 06:50:58 +0530

    Time: 1 ms (87.24%), Space: 44 MB (87.89%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
-        gen(nums,0,ans,new ArrayList<>());
+        List<Integer> list= new ArrayList<>();
+        fun(0,nums,ans,list);
         return ans;
     }
-    public void gen(int []nums,int start,List<List<Integer>> ans,List<Integer>list){
+    public void fun(int start,int[]nums,List<List<Integer>>ans,List<Integer>list){
         if(start==nums.length){
             ans.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[start]);
-        gen(nums,start+1,ans,list);
+        fun(start+1,nums,ans,list);
         list.remove(list.size()-1);
-        gen(nums,start+1,ans,list);
+        fun(start+1,nums,ans,list);
     }
 }
