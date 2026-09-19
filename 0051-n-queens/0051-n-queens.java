6bc298262ab3194dc80b950eb464a61613aa9ae2
Kunal Sharma <kunal.sharma_cs24@gla.ac.in>
2026-09-19 23:26:06 +0530

    Time: 3 ms (37.16%), Space: 46.7 MB (45.85%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans= new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
         char [][]board=new char[n][n];
         for(int i=0;i<n;i++){
             Arrays.fill(board[i],'.');
         }
-        solve(board,0,n,ans,new ArrayList<>());
+        fun(board,0,n,ans,new ArrayList<>());
         return ans;
     }
-    public void solve(char[][]board,int row,int n,List<List<String>>ans,List<String>list){
+    public void fun(char [][]board,int row,int n,List<List<String>>ans,List<String>list){
         if(row==n){
             ans.add(new ArrayList<>(list));
             return;
@@ -17,20 +17,20 @@ class Solution {
             if(check(board,row,i)){
                 board[row][i]='Q';
                 list.add(new String(board[row]));
-                solve(board,row+1,n,ans,list);
+                fun(board,row+1,n,ans,list);
                 list.remove(list.size()-1);
-                board[row][i]='.';
+                board[row][i]='.';  
             }
         }
     }
-    public boolean check(char[][]board,int row,int col){
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q') return false;
+    public boolean check(char[][]board,int r, int c){
+        for(int i=r-1;i>=0;i--){
+            if(board[i][c]=='Q') return false;
         }
-        for(int i=row-1,j=col+1;i>=0&&j<board[0].length;i--,j++){
+        for(int i=r-1,j=c+1;i>=0 &&  j<board.length;i--,j++){
             if(board[i][j]=='Q') return false;
         }
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+        for(int i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
             if(board[i][j]=='Q') return false;
         }
         return true;
